935661905bd3015f773c96fe0668ff9b55d8413e
satish-max-collab <satish090490@kashiit.ac.in>
2025-12-24 12:07:44 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { AuthProvider } from "./context/AuthContext_old";
+import { AuthProvider } from "./context/AuthContext";
 
 // Optional: Material-UI baseline styles
 import { CssBaseline } from "@mui/material";
